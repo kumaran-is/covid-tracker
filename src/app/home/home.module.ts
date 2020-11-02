ae8ae12192e5b18be07ba446fcdc42cca8c08f4e
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2020-11-02 17:05:30 -0500

    added routes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
-import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from './../shared/shared.module';
+import { HomeComponent } from './home.component';
 
 @NgModule({
-  declarations: [HomeRoutingModule.components],
-  imports: [
-    SharedModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent],
+  imports: [SharedModule]
 })
 export class HomeModule { }
