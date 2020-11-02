b36157f289e781f2b3dbc2521c8a7d3889f0dac8
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2020-11-01 21:27:07 -0500

    added ag-grid

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AgGridModule } from 'ag-grid-angular';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    AgGridModule.withComponents([])
   ]
 })
 export class SharedModule { }
