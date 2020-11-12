32299ece646be24f5e2a01f4232218a8322da8ac
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2020-11-12 11:50:06 -0500

    updated karma.conf file

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, './coverage/covid-tracker'),
       reports: ['html', 'lcovonly', 'text', 'cobertura'],
       fixWebpackSourcePaths: true,
